1a60e27effd8aa28575a2a0b2a6cb10308d903dd
elvinceffm <56054250+elvinceffm@users.noreply.github.com>
2022-08-24 01:36:02 +0200

    heroku url added to settings.py: 	modified:   manage.py 	modified:   mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-19v99rp2@s-=-!1%m(nn!wyt!xe4&&n02%foi9e6%l0h-viu0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.heroku.com']    # heroku instead of pythonanywhere
 
 
 # Application definition
