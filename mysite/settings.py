43b6da8cfa0ab47cdd3c814fb86d07addecb840c
elvinceffm <56054250+elvinceffm@users.noreply.github.com>
2022-08-24 11:54:23 +0200

    modified:   mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-19v99rp2@s-=-!1%m(nn!wyt!xe4&&n02%foi9e6%l0h-viu0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'tranquil-fjord-25074.herokuapp.com']    # heroku instead of pythonanywhere # added app's sub url
+ALLOWED_HOSTS = ['127.0.0.1', 'emilprause.pythonanywhere.com']    # heroku instead of pythonanywhere # added app's sub url
+# tranquil-fjord-25074.herokuapp.com
 
 
 # Application definition
